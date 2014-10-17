37209f1317c1017e2cbd3603169db48224b29952
David Anderson <dvander@alliedmods.net>
2014-10-17 00:25:46 -0700

    Add a travis script.

---
+++ checkout-deps.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# This should be run inside a folder that contains sourcemod, otherwise, it will checkout things into "sm-dependencies".
+
+trap "exit" INT
+
+checkout ()
+{
+  if [ ! -d "$name" ]; then
+    git clone $repo -b $branch $name
+    if [ -n "$origin" ]; then
+      cd $name
+      git remote rm origin
+      git remote add origin $origin
+      cd ..
+    fi
+  else
+    cd $name
+    git checkout $branch
+    git pull origin $branch
+    cd ..
+  fi
+}
+
+`python -c "import ambuild2"`
+if [ $? -eq 1 ]; then
+  repo="https://github.com/alliedmodders/ambuild"
+  origin=
+  branch=master
+  name=ambuild
+  checkout
+
+  cd ambuild
+  if [ $iswin -eq 1 ]; then
+    python setup.py install
+  else
+    python setup.py build
+    echo "About to install AMBuild - press Ctrl+C to abort, otherwise enter your password for sudo."
+    sudo python setup.py install
+  fi
+fi
