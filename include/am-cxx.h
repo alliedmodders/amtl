870938d377acb70f814fb32ebceded48ac9e9dad
David Anderson <dvander@alliedmods.net>
2015-07-19 13:33:35 -0700

    Add equality overloads for typed enums, and add some MSVC sugar.

--- include/am-cxx.h
+++ include/am-cxx.h
@@ -184,6 +184,18 @@
 # endif
 #endif
 
+#if defined(_MSC_VER)
+// This feature has been around for long enough that we shouldn't have to
+// warn for it.
+# pragma warning(disable: 4351)
+#endif
+
+#if defined(_MSC_VER)
+# define KE_LINKONCE(x) __declspec(selectany) x
+#else
+# define KE_LINKONCE(x) x __attribute__((weak))
+#endif
+
 #define KE_DEFINE_ENUM_OPERATORS(EnumName)                                          \
   static inline EnumName operator |(const EnumName &left, const EnumName &right) {  \
     return EnumName(uint32_t(left) | uint32_t(right));                              \
@@ -208,6 +220,20 @@
   }                                                                                 \
   static inline bool operator !(const EnumName &obj) {                              \
     return uint32_t(obj) == 0;                                                      \
+  }
+
+#define KE_DEFINE_ENUM_COMPARATORS(EnumName, Type)                                  \
+  static inline bool operator ==(const EnumName& left, const Type& right) {         \
+    return Type(left) == right;                                                     \
+  }                                                                                 \
+  static inline bool operator ==(const Type& left, const EnumName& right) {         \
+    return left == Type(right);                                                     \
+  }                                                                                 \
+  static inline bool operator !=(const EnumName& left, const Type& right) {         \
+    return Type(left) != right;                                                     \
+  }                                                                                 \
+  static inline bool operator !=(const Type& left, const EnumName& right) {         \
+    return left != Type(right);                                                     \
   }
 
 #endif // _include_amtl_cxx_support_h_
