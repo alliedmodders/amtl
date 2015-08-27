e688eabaa200dc550c0158bc9918e94fdd27d698
David Anderson <dvander@alliedmods.net>
2015-08-27 16:45:46 -0400

    Add simple filesystem helpers.

---
+++ amtl/os/am-fsutil-posix.h
@@ -0,0 +1,72 @@
+// vim: set sts=8 ts=2 sw=2 tw=99 et:
+//
+// Copyright (C) 2013-2015, David Anderson and AlliedModders LLC
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  * Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//  * Neither the name of AlliedModders LLC nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#ifndef _include_amtl_os_fsutil_posix_h_
+#define _include_amtl_os_fsutil_posix_h_
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+namespace ke {
+namespace file {
+
+static inline bool
+PathExists(const char* path)
+{
+  struct stat s;
+  return (stat(path, &s) == 0);
+}
+
+static inline bool
+IsFile(const char* path)
+{
+  struct stat s;
+  if (stat(path, &s) != 0)
+    return false;
+  return S_ISREG(s.st_mode) ? true : false;
+}
+
+static inline bool
+IsDirectory(const char* path)
+{
+  struct stat s;
+  if (stat(path, &s) != 0)
+    return false;
+  return S_ISDIR(s.st_mode) ? true : false;
+}
+
+static inline bool
+CreateDirectory(const char* path, unsigned mode=0755)
+{
+  return mkdir(path, mode) == 0;
+}
+
+} // namespace file
+} // namespace ke
+
+#endif // _include_amtl_os_fsutil_posix_h_
---
+++ amtl/os/am-fsutil-windows.h
@@ -0,0 +1,74 @@
+// vim: set sts=8 ts=2 sw=2 tw=99 et:
+//
+// Copyright (C) 2013-2015, David Anderson and AlliedModders LLC
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  * Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//  * Neither the name of AlliedModders LLC nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#ifndef _include_amtl_os_fsutil_windows_h_
+#define _include_amtl_os_fsutil_windows_h_
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <direct.h>
+
+namespace ke {
+namespace file {
+
+static inline bool
+PathExists(const char* path)
+{
+  return GetFileAttributesA(path) != INVALID_FILE_ATTRIBUTES;
+}
+
+static inline bool
+IsFile(const char* path)
+{
+  DWORD attr = GetFileAttributesA(path);
+  if (attr == INVALID_FILE_ATTRIBUTES)
+    return false;
+  if (attr & (FILE_ATTRIBUTE_DIRECTORY|FILE_ATTRIBUTE_DEVICE))
+    return false;
+  return true;
+}
+
+static inline bool
+IsDirectory(const char* path)
+{
+  DWORD attr = GetFileAttributesA(path);
+  if (attr == INVALID_FILE_ATTRIBUTES)
+    return false;
+  return (attr & (FILE_ATTRIBUTE_DIRECTORY|FILE_ATTRIBUTE_DEVICE)) == FILE_ATTRIBUTE_DIRECTORY;
+}
+
+static inline bool
+CreateDirectory(const char* path, unsigned mode_IGNORED=0)
+{
+  return mkdir(path) == 0;
+}
+
+} // namespace file
+} // namespace ke
+
+#endif // _include_amtl_os_fsutil_windows_h_
