c0bd422a7ee465e22db0366e19466e8f99d728c1
WildCard65 <lollol222gg@gmail.com>
2020-07-31 13:03:41 -0400

    Updated 'checkout-deps' to check for 'pip'/'pip3' prior to installing 'AMBuild'

--- .appveyor/build.bat
+++ .appveyor/build.bat
@@ -2,6 +2,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 mkdir opt32 && cd opt32
 C:\python27\python.exe ..\configure.py --enable-optimize
-C:\python27\python.exe C:\python27\Scripts\ambuild
+ambuild
 cd ..
 opt32\tests\testrunner\testrunner.exe
