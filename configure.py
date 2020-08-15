c45ec0f8462b9c03c5447d5ee9ad5fcd58ca42c1
David Anderson <dvander@alliedmods.net>
2020-08-15 15:17:35 -0700

    Switch to AMBuild 2.2.

--- configure.py
+++ configure.py
@@ -12,10 +12,16 @@ except:
 		sys.stderr.write('http://www.alliedmods.net/ambuild\n')
 	sys.exit(1)
 
-parser = run.BuildParser(sourcePath=sys.path[0], api='2.1')
-parser.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
-                          help='Enable debugging symbols')
-parser.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
-                          help='Enable optimization')
-parser.options.add_option('--std', type=str, default=None, help='Override C++ standard')
+# Hack to show a decent upgrade message, which wasn't done until 2.2.
+ambuild_version = getattr(run, 'CURRENT_API', '2.1')
+if ambuild_version.startswith('2.1'):
+	sys.stderr.write("AMBuild 2.2 or higher is required; please update\n")
+	sys.exit(1)
+
+parser = run.BuildParser(sourcePath=sys.path[0], api='2.2')
+parser.options.add_argument('--enable-debug', action='store_const', const='1', dest='debug',
+                            help='Enable debugging symbols')
+parser.options.add_argument('--enable-optimize', action='store_const', const='1', dest='opt',
+                            help='Enable optimization')
+parser.options.add_argument('--std', type=str, default=None, help='Override C++ standard')
 parser.Configure()
